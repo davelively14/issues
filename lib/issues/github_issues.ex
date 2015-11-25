b04d599c5ed3d8032c298c4e82cd3cf2c03aec4a
Dave Lively <davelively14@gmail.com>
2015-11-24 21:29:40 -0500

    Added logger functionality

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,11 +1,15 @@
 defmodule Issues.GithubIssues do
+
+  require Logger
+
   @user_agent [ {"User-agent", "Elixir dave@pragprog.com"} ]
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
     issues_url(user, project)
-      |> HTTPoison.get(@user_agent)
-      |> handle_response
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
   end
 
   def issues_url(user, project) do
@@ -13,10 +17,13 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({:ok, %HTTPoison.Response{body: body}}) do
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
     { :ok, :jsx.decode(body) }
   end
 
-  def handle_response({ _,  %HTTPoison.Response{body: body}}) do
+  def handle_response({ _status,  %HTTPoison.Response{body: body}}) do
+    Logger.error "Error #{status} returned"
     { :error, :jsx.decode(body) }
   end
 end
